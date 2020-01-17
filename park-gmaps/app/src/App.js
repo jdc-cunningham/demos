fe688a349f6fa9eb93fa865a56b2759652b46121
jdc-cunningham <jcun4128@gmail.com>
2020-01-16 18:02:26 -0600

    updated side panel message

--- park-gmaps/app/src/App.js
+++ park-gmaps/app/src/App.js
@@ -267,7 +267,7 @@ function App() {
 				<h1>Park Finder Demo</h1>
 				<p>After you search a location, to reset, click on "Search Parks Near Me"</p>
 				<br/>
-				<p>Add parks manually is not built yet at this time, it would just be a basic coordinate picker</p>
+				<p>Add parks manually is just a basic coordinate picker</p>
 				<br/>
 				<p>Note: the delay while the parks load is from the pagination, there is a mandatory 2 second delay between requests. Can do it in the background/show all at once, show loading icon, etc...</p>
 			</>
